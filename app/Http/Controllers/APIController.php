de6ee64e9b9db094dac60228305040e7b6378783
Deric <dericlima@gmail.com>
2021-09-13 13:47:03 +0200

    Code refactoring in the API Controller

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -28,27 +28,58 @@ class APIController extends Controller
             return ['Validation error' => $exception->getMessage()];
         }
 
+        $result = [];
         foreach ($request->get('services') as $service) {
             switch ($service) {
                 case 'sms':
-                    try {
-                        $messageProvider = new TwilioMessageProvider();
-                        $messageProvider->sendMessage($request);
-                    } catch (\Exception $exception) {
-                        return ['Request error' => $exception->getMessage()];
-                    }
+                    $result[] = $this->sendSMSMessage($request);
                     break;
                 case 'email':
-                    try {
-                        $messageProvider = new MailGunMessageProvider();
-                        $messageProvider->sendMessage($request);
-                    } catch (\Exception $exception) {
-                        return ['Request error' => $exception->getMessage()];
-                    }
+                    $result[] = $this->sendEmailMessage($request);
                     break;
             }
         }
 
-        return $request->all();
+        return $result;
+    }
+
+    private function sendSMSMessage(Request $request): array
+    {
+        $result = [];
+        try {
+            $messageProvider = new TwilioMessageProvider();
+            $messageProvider->sendMessage($request);
+            $result[] =[
+                'Service'   => 'SMS',
+                'Success'   => 'SMS sent with success'
+            ];
+        } catch (\Exception $exception) {
+            $result[] =[
+                'Service'       => 'SMS',
+                'Request error' => $exception->getMessage()
+            ];
+        }
+
+        return $result;
+    }
+
+    private function sendEmailMessage(Request $request): array
+    {
+        $result = [];
+        try {
+            $messageProvider = new MailGunMessageProvider();
+            $messageProvider->sendMessage($request);
+            $result[] =[
+                'Service'   => 'Email',
+                'Success'   => 'Email sent with success'
+            ];
+        } catch (\Exception $exception) {
+            $request[] =[
+                'Service'       => 'Email',
+                'Request error' => $exception->getMessage()
+            ];
+        }
+
+        return $result;
     }
 }
